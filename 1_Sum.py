bc53782bc35a6536d651d6ef82271f62c993d5d0
Nitesh <nitesh@fireshark.in>
2023-08-15 22:47:55 +0530

    MOdify sum code

--- 1_Sum.py
+++ 1_Sum.py
@@ -1,6 +1,6 @@
 # Get input from the user
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 # Calculate the sum
 sum_result = num1 + num2
