0eaeb4487fb75572ec79b3116c918358c5e968b9
Nitesh <nitesh@fireshark.in>
2023-08-15 22:48:28 +0530

    ADD multiply

---
+++ 3_Multiply.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the product
+product_result = num1 * num2
+
+# Display the result
+print("The product of", num1, "and", num2, "is:", product_result)
