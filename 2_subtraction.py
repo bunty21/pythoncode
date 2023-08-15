7f32e6db090dbdca4f22ab5ff81025d7968ecced
Nitesh <nitesh@fireshark.in>
2023-08-15 22:45:35 +0530

    variables change in subtraction

---
+++ 2_subtraction.py
@@ -0,0 +1,9 @@
+# Get input from the user
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+
+# Calculate the subtraction
+subtraction_result = A - B
+
+# Display the result
+print("The subtraction of", A, "and", B, "is:", subtraction_result)
